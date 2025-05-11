a943a6ec3ebfbf29d767a3d9ebe1b824fe8a9c15
Victor River <victor@devx.com.au>
2025-05-11 22:29:20 +0700

    Improve comment

--- lib/ui/shared/utils.dart
+++ lib/ui/shared/utils.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 class Utils {
-  //Hàm loại bỏ dấu Tiếng Việt để dễ dàng trong tìm kiếm
+  //Function to remove Vietnamese accents for easier searching
   static String removeVietnameseAccent(String origin) {
     Map<String, List<String>> template = {
       'a': [
@@ -54,24 +54,24 @@ class Utils {
     return newString;
   }
 
-  //Hàm bổ trợ log thông tin ra màn hình để debug với thông tin về file và dòng
+  //Helper function to log information to screen for debugging with file and line info
   static void logMessage(String message) {
-    //Lấy thông tin về thực thị trong stack tại thời điểm gọi hàm này.
-    //Thông tin stack trace như sau: #1.....................
+    //Get execution information in the stack at the time this function is called.
+    //Stack trace information is as follows: #1.....................
     //#2.....................
     //vvv.v..vvv
-    //Nó giống như là một stack biểu thị thứ tự thực thi của từng thần phần trong hệ
-    //thống khi gọi hàm này, mỗi dòng được đánh dấu bằng ký tự # và số thứ tự
-    //#0 chính là hàm logMessage bởi vì nó phải truy xuất đến người gọi nó trước rồi mới
-    //tới nó cuối cùng. Có thể hiểu là thứ tự thực hiện các lệnh từ lệnh nhỏ nhất
-    //tới lệnh lớn nhất.
+    //It's like a stack showing the execution order of each component in the system
+    //when calling this function, each line is marked with # and sequence number
+    //#0 is the logMessage function itself because it has to access its caller first
+    //before getting to itself last. Can be understood as the order of execution from
+    //smallest to largest command.
     final stackTrace = StackTrace.current;
-    //Chuyển thông tin stack trace thành một chuỗi và tách thành các phần tử
+    //Convert stack trace information to a string and split into elements
     final frames = stackTrace.toString().split('\n');
-    //Nếu có nhiều hơn 1 phần tử thì lấy phần tử thứ 2
+    //If there is more than 1 element, take the second element
     if (frames.length > 1) {
-      //Phần tử thứ hai chính là nơi nó được gọi gần nhất, thí dụ, nếu trong file main
-      //gọi hàm logMessage thì phần tử thứ 2 sẽ là thông tin đường dẫn file main và dòng
+      //The second element is where it was called most recently, for example, if in the main file
+      //calls logMessage then the second element will be the main file path and line information
       final frame = frames[1];
       final fileInfo = _extractFileInfo(frame);
       log('$message ($fileInfo)\n');
@@ -80,7 +80,7 @@ class Utils {
     }
   }
 
-  //Hàm lấy thông tin về file và dòng của phần tử thứ 2 trong stack trace
+  //Function to get file and line information of the second element in stack trace
   static String _extractFileInfo(String frame) {
     //r'...': The r before the string indicates a raw string in Dart, which means special characters like backslashes are treated as literals.
     //(\S+\.dart):(\d+):(\d+): This part of the pattern matches a file name with an extension (like .dart), followed by a colon, a line number, another colon, and a column number.
@@ -88,11 +88,11 @@ class Utils {
     //The :(\d+):(\d+) matches a colon, followed by one or more digits (representing the line number), another colon, and one or more digits (representing the column number).
     //`(...)`: Parentheses create a capturing group, which allows us to extract the matched column number.
     final fileInfoPattern = RegExp(r'(\S+\.dart):(\d+):(\d+)');
-    //Tìm kiếm mẫu trong chuỗi frame
+    //Search for pattern in frame string
     final match = fileInfoPattern.firstMatch(frame);
     if (match != null) {
-      //Mỗi ngoặc tròn là một nhóm, mỗi nhóm sẽ được lưu trữ trong một list
-      //Ví dụ: match.group(0) sẽ là toàn bộ chuỗi khớp, match.group(1) sẽ là phần tử thứ nhất trong list, match.group(2) sẽ là phần tử thứ hai trong list, vv...
+      //Each parenthesis is a group, each group will be stored in a list
+      //Example: match.group(0) will be the entire matching string, match.group(1) will be the first element in the list, match.group(2) will be the second element in the list, etc...
       final fileName = match.group(1);
       final lineNumber = match.group(2);
       return '$fileName:$lineNumber';
