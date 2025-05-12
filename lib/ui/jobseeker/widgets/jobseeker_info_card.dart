9d2162f4f3e12cbce8f20565eb1fbcf8254498e5
Victor River <victor@devx.com.au>
2025-05-12 11:29:19 +0700

    Refactor code

--- lib/ui/jobseeker/widgets/jobseeker_info_card.dart
+++ lib/ui/jobseeker/widgets/jobseeker_info_card.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class JobseekerInfoCard extends StatelessWidget {
-  const JobseekerInfoCard(
-      {required this.title,
-      this.iconButton,
-      required this.children,
-      super.key});
+  const JobseekerInfoCard({
+    Key? key,
+    required this.title,
+    this.iconButton,
+    required this.children,
+  }) : super(key: key);
 
   final List<Widget> children;
   final IconButton? iconButton;
@@ -16,60 +17,41 @@ class JobseekerInfoCard extends StatelessWidget {
     final deviceSize = MediaQuery.of(context).size;
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
+
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       width: deviceSize.width - 30,
-      // height: 300,
       decoration: BoxDecoration(
         color: theme.colorScheme.surface,
         borderRadius: BorderRadius.circular(10),
         boxShadow: [
-          // BoxShadow(
-          //   color: Colors.grey.shade200,
-          //   spreadRadius: 4,
-          //   blurRadius: 2,
-          //   offset: const Offset(0, 0),
-          // )
           BoxShadow(
             color: Colors.grey.withOpacity(0.1),
             spreadRadius: 4,
             blurRadius: 1,
-            offset: const Offset(0, 0), // changes position of shadow
+            offset: const Offset(0, 0),
           ),
         ],
       ),
       child: Column(
         children: [
           if (iconButton == null)
-            const SizedBox(
-              height: 10,
-            ),
-          //Dòng đầu tiên tiêu đề và nút chỉnh sửa thông tin cá nhân
+            const SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //Tiêu đề thông tin cá nhân
               Text(
                 title,
-                style:
-                    textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
+                style: textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
               ),
-              //Nút hành động
-              if (iconButton != null) iconButton!
+              if (iconButton != null) iconButton!,
             ],
           ),
           if (iconButton == null)
-            const SizedBox(
-              height: 10,
-            ),
-          Divider(
-            thickness: 1,
-          ),
-          //Các con được thêm vào
+            const SizedBox(height: 10),
+          const Divider(thickness: 1),
           ...children,
-          const SizedBox(
-            height: 20,
-          )
+          const SizedBox(height: 20),
         ],
       ),
     );
--- lib/ui/jobseeker/widgets/message_icon.dart
+++ lib/ui/jobseeker/widgets/message_icon.dart
@@ -5,21 +5,8 @@ import 'package:job_finder_app/ui/shared/utils.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
-class MessageIcon extends StatefulWidget {
-  const MessageIcon({super.key});
-
-  @override
-  State<MessageIcon> createState() => _MessageIconState();
-}
-
-class _MessageIconState extends State<MessageIcon> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await context.read<MessageManager>().getAllConversation();
-    });
-  }
+class MessageIcon extends StatelessWidget {
+  const MessageIcon({Key? key}) : super(key: key);
 
   void _gotoConversationList(BuildContext context) {
     context.pushNamed('conversation-list');
@@ -30,29 +17,28 @@ class _MessageIconState extends State<MessageIcon> {
     Utils.logMessage('MessageIcon build');
     final theme = Theme.of(context);
     final isEmployer = context.read<AuthManager>().isEmployer;
-    return Selector<MessageManager, int>(
-        selector: (context, messageManager) => isEmployer
-            ? messageManager.unseenEmployerMessages
-            : messageManager.unseenJobseekerMessages,
-        builder: (context, unseenMessages, child) {
-          return FloatingActionButton(
-            onPressed: () => _gotoConversationList(context),
-            backgroundColor: Colors.blueAccent.shade400,
-            shape: CircleBorder(),
-            child: unseenMessages > 0
-                ? Badge(
-                    label: Text(
-                        unseenMessages > 9 ? '9+' : unseenMessages.toString()),
-                    child: Icon(
-                      Icons.message,
-                      color: theme.indicatorColor,
-                    ),
-                  )
-                : Icon(
-                    Icons.message,
-                    color: theme.indicatorColor,
-                  ),
-          );
-        });
+    final messageManager = context.read<MessageManager>();
+    final unseenMessages = isEmployer
+        ? messageManager.unseenEmployerMessages
+        : messageManager.unseenJobseekerMessages;
+
+    return FloatingActionButton(
+      onPressed: () => _gotoConversationList(context),
+      backgroundColor: Colors.blueAccent.shade400,
+      shape: CircleBorder(),
+      child: unseenMessages > 0
+          ? Badge(
+              label: Text(
+                  unseenMessages > 9 ? '9+' : unseenMessages.toString()),
+              child: Icon(
+                Icons.message,
+                color: theme.indicatorColor,
+              ),
+            )
+          : Icon(
+              Icons.message,
+              color: theme.indicatorColor,
+            ),
+    );
   }
 }
